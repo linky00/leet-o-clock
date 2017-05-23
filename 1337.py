7d53729ce37c4250354517a386ec61e092f1eeae
linky00 <linky00@plotholestudios.com>
2017-05-23 21:01:39 +0100

    Bugfixes, uses smaller gif

--- 1337.py
+++ 1337.py
@@ -8,25 +8,36 @@ CONSUMER_SECRET = keys[1].rstrip()
 ACCESS_TOKEN = keys[2].rstrip()
 ACCESS_TOKEN_SECRET = keys[3].rstrip()
 
+print("Authenticating...")
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-safygiphy = safygiphy.Giphy()
+giphy = safygiphy.Giphy()
 
 attempts = 3
 success = False
 
 while success == False:
-	gif = safygiphy.trending()
-	urllib.request.urlretrieve(gif['data'][0]['images']['original']['url'], 'temp_gif.gif')
+	print("Getting trending gifs...")
+	gif = giphy.trending()
+	print("Downloading gif...")
+	urllib.request.urlretrieve(gif['data'][0]['images']['downsized']['url'], 'temp_gif.gif')
 
 	try:
+		print("Uploading tweet...")
 		api.update_with_media('temp_gif.gif', """It's leet o'clock!""")
-	except tweepy.TweepError:
+	except tweepy.TweepError as error:
+		print("Something went wrong!")
+		print(error)
 		pass
 	else:
+		print("Success!")
 		success = True
 	
-	attempts -= 1
 	os.remove('temp_gif.gif')
+	attempts -= 1
+	print("Remaining attempts: " + str(attempts))
+	if attempts <= 0:
+		print("Giving up.")
+		success = True
